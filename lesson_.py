4a0ba7e4d26ad62ce351491aa42460db90321526
santiago cardenas <scardenas@brooklynsteamcenter.org>
2024-09-16 10:48:25 -0400

    exparamint

---
+++ lesson_.py
@@ -0,0 +1,29 @@
+#step one 
+print("santiago")
+#step two name your favorit show 
+my_fav_show = "skibidi_toilet"
+
+("x = 10")
+
+print="100 x 10"
+
+food_list = ["pizza","cheese_cake","taco"]
+
+age=0
+if age  >= 18: 
+    print("they are able to get their drivers license")
+
+elif:
+print("they arent able to get thier license")
+
+name = "your name here "
+print(name)
+#9 create a for loop that prints each of your favorite foods 
+my_foods =["pizza","burger","apple"]
+
+for food in my_foods:
+    print(food)
+#step 10 create a while loop that will never run 
+
+while false :
+print("will never exitcute")
